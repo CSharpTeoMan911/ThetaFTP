184c50b10d646980058fbb8c4d1026ef0ce70a5b
Teodor Mihail <teodormihail07@gmail.com>
2024-08-03 23:03:34 +0100

    * BEGAN THE AUTHENTICATION BACKEND

--- Shared/Controllers/AuthenticationController.cs
+++ Shared/Controllers/AuthenticationController.cs
@@ -1,9 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ThetaFTP.Shared.Classes;
+using ThetaFTP.Shared.Models;
+
 
 namespace ThetaFTP.Shared.Controllers
 {
-    public class AuthenticationController : Controller
+    [Route("/authentication")]
+    public class AuthenticationController : Controller, CRUD_Interface<AuthenticationModel, AuthenticationModel, AuthenticationModel, AuthenticationModel>
     {
+        [HttpDelete("delete")]
+        public async Task<string?> Delete([FromQuery] AuthenticationModel? value)
+        {
+            string? respose = String.Empty;
 
+            return respose;
+        }
+
+        [HttpGet("get")]
+        public async Task<string?> Get([FromQuery] AuthenticationModel? value)
+        {
+            string? respose = String.Empty;
+
+            return respose;
+        }
+
+        [HttpPost("insert")]
+        public async Task<string?> Insert([FromQuery] AuthenticationModel? value)
+        {
+            string? respose = String.Empty;
+
+            return respose;
+        }
+
+        [HttpPut("update")]
+        public async Task<string?> Update([FromQuery] AuthenticationModel? value)
+        {
+            string? respose = String.Empty;
+
+            return respose;
+        }
     }
 }
