55a117deb6589e16f64a0951ff726b2414623f1e
Teodor Mihail <teodormihail07@gmail.com>
2024-08-06 01:32:05 +0100

    * INIT DATABASE CONTROLLER

---
+++ Shared/Controllers/DatabaseAuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using ThetaFTP.Shared.Classes;
+using ThetaFTP.Shared.Models;
+
+namespace ThetaFTP.Shared.Controllers
+{
+    public class DatabaseAuthenticationController : CRUD_Interface<AuthenticationModel, AuthenticationModel, AuthenticationModel, AuthenticationModel>
+    {
+        public Task<string?> Delete(AuthenticationModel? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> Get(AuthenticationModel? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> Insert(AuthenticationModel? value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> Update(AuthenticationModel? value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Shared/Shared.cs
+++ Shared/Shared.cs
@@ -1,6 +1,11 @@
-﻿namespace ThetaFTP.Shared
+﻿using ThetaFTP.Shared.Classes;
+using ThetaFTP.Shared.Controllers;
+using ThetaFTP.Shared.Models;
+
+namespace ThetaFTP.Shared
 {
     public class Shared
     {
+        public static CRUD_Strategy<AuthenticationModel, AuthenticationModel, AuthenticationModel, AuthenticationModel> database_auth = new CRUD_Strategy<AuthenticationModel, AuthenticationModel, AuthenticationModel, AuthenticationModel>(new DatabaseAuthenticationController());
     }
 }
