c2ea18e6ddfe994a6cb9b71c66e2f3c36e29a25c
Teodor Mihail <teodormihail07@gmail.com>
2024-08-30 19:37:34 +0100

    * FINISHED FILE DETAILS FUNCTION

--- Shared/Classes/ServerLoggerService.cs
+++ Shared/Classes/ServerLoggerService.cs
@@ -1,30 +1,54 @@
 ﻿namespace ThetaFTP.Shared.Classes
 {
+    using System.Text;
+    using ThetaFTP.Shared.Formatters;
+
     public class ServerLoggerService
     {
         private static string ErrorLogsFile = "ErrorLogs.txt";
 
         
-        public static async Task WriteErrorLog()
+        public static async Task WriteErrorLog(string error_message, Type class_name, string method_name)
         {
             try
             {
+                FileSystemFormatter.CreateLogsDir();
 
-            }
-            catch
-            {
+                StringBuilder error_log = new StringBuilder(Environment.CurrentDirectory).Append("ServerLogs").Append(FileSystemFormatter.PathSeparator()).Append(DateTime.Now.ToString()).Append("_").Append(ErrorLogsFile);
 
-            }
-            finally
-            {
+                FileStream errorLogStream = File.OpenWrite(error_log.ToString());
+                try
+                {
+                    error_log.Clear();
 
+                    error_log.Append("Error name: ").Append(error_message).Append("\n");
+
+                    error_log.Append("Class name: ").Append(class_name.Name).Append("\n");
+
+                    error_log.Append("Method name: ").Append(method_name).Append("\n");
+
+                    byte[] error_log_text_as_bytes = Encoding.UTF8.GetBytes(error_log.ToString());
+
+                    await errorLogStream.WriteAsync(error_log_text_as_bytes, 0, error_log_text_as_bytes.Length);
+                    await errorLogStream.FlushAsync();
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    await errorLogStream.DisposeAsync();
+                }
             }
+            catch { }
         }
 
         public static async Task DeleteErrorLogFile()
         {
             try
             {
+
 
             }
             catch
