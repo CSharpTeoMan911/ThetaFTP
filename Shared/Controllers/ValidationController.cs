21f0950986760c25120ade7df27f23313bc48bd9
Teodor Mihail <teodormihail07@gmail.com>
2025-03-01 04:00:12 +0000

    * Re-formated the API payload structure * Fixed authentication bugs * Improved performance

--- Shared/Controllers/ValidationController.cs
+++ Shared/Controllers/ValidationController.cs
@@ -11,88 +11,129 @@ namespace ThetaFTP.Shared.Controllers
     public class ValidationController : Controller
     {
         [HttpGet("validate-account")]
-        public async Task<string?> GetAccount([FromQuery] ValidationModel? value)
+        public async Task<ActionResult?> GetAccount([FromQuery] ValidationModel? value)
         {
-            string? result = String.Empty;
+            PayloadModel? payloadModel = new PayloadModel();
 
             if (Shared.configurations != null)
                 if (!Shared.configurations.use_firebase)
                 {
-                    result = await Shared.database_validation.ValidateAccount(value);
+                    payloadModel = await Shared.database_validation.ValidateAccount(value);
                 }
                 else
                 {
-                    result = await Shared.firebase_database_validation.ValidateAccount(value);
+                    payloadModel = await Shared.firebase_database_validation.ValidateAccount(value);
                 }
-            return result;
+
+            if (payloadModel?.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Ok(payloadModel);
+            }
+            else
+            {
+                return StatusCode(500, payloadModel);
+            }
         }
 
         [HttpGet("validate-code")]
-        public async Task<string?> GetLogInCode([FromQuery] ValidationModel? value)
+        public async Task<ActionResult?> GetLogInCode([FromQuery] ValidationModel? value)
         {
-            string? result = String.Empty;
+            PayloadModel? payloadModel = new PayloadModel();
 
             if (Shared.configurations != null)
                 if (!Shared.configurations.use_firebase)
                 {
-                    result = await Shared.database_validation.ValidateLogInSession(value);
+                    payloadModel = await Shared.database_validation.ValidateLogInSession(value);
                 }
                 else
                 {
-                    result = await Shared.firebase_database_validation.ValidateLogInSession(value);
+                    payloadModel = await Shared.firebase_database_validation.ValidateLogInSession(value);
                 }
-            return result;
+
+            if (payloadModel?.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Ok(payloadModel);
+            }
+            else
+            {
+                return StatusCode(500, payloadModel);
+            }
         }
 
         [HttpDelete("delete-session")]
-        public async Task<string?> DeleteLogInSession([FromQuery] string? value)
+        public async Task<ActionResult?> DeleteLogInSession([FromQuery] string? value)
         {
-            string? result = String.Empty;
+            PayloadModel? payloadModel = new PayloadModel();
 
             if (Shared.configurations != null)
                 if (!Shared.configurations.use_firebase)
                 {
-                    result = await Shared.database_validation.DeleteLogInSession(value);
+                    payloadModel = await Shared.database_validation.DeleteLogInSession(value);
                 }
                 else
                 {
-                    result = await Shared.firebase_database_validation.DeleteLogInSession(value);
+                    payloadModel = await Shared.firebase_database_validation.DeleteLogInSession(value);
                 }
-            return result;
+
+            if (payloadModel?.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Ok(payloadModel);
+            }
+            else
+            {
+                return StatusCode(500, payloadModel);
+            }
         }
 
         [HttpDelete("delete-account")]
-        public async Task<string?> DeleteAccount([FromQuery] string? value)
+        public async Task<ActionResult?> DeleteAccount([FromQuery] string? value)
         {
-            string? result = String.Empty;
+            PayloadModel? payloadModel = new PayloadModel();
 
             if (Shared.configurations != null)
                 if (!Shared.configurations.use_firebase)
                 {
-                    result = await Shared.database_validation.ValidateAccountDeletion(value);
+                    payloadModel = await Shared.database_validation.ValidateAccountDeletion(value);
                 }
                 else
                 {
-                    result = await Shared.firebase_database_validation.ValidateAccountDeletion(value);
+                    payloadModel = await Shared.firebase_database_validation.ValidateAccountDeletion(value);
                 }
-            return result;
+
+            if (payloadModel?.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Ok(payloadModel);
+            }
+            else
+            {
+                return StatusCode(500, payloadModel);
+            }
         }
 
         [HttpPut("update-account")]
-        public async Task<string?> UpdateAccount([FromQuery] PasswordUpdateValidationModel? value)
+        public async Task<ActionResult?> UpdateAccount([FromQuery] PasswordUpdateValidationModel? value)
         {
+            PayloadModel? payloadModel = new PayloadModel();
             string? result = String.Empty;
 
             if (Shared.configurations != null)
                 if (!Shared.configurations.use_firebase)
                 {
-                    result = await Shared.database_validation.ValidateAccountUpdate(value);
+                    payloadModel = await Shared.database_validation.ValidateAccountUpdate(value);
                 }
                 else
                 {
-                    result = await Shared.firebase_database_validation.ValidateAccountUpdate(value);
+                    payloadModel = await Shared.firebase_database_validation.ValidateAccountUpdate(value);
                 }
-            return result;
+
+            if (payloadModel?.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return Ok(payloadModel);
+            }
+            else
+            {
+                return StatusCode(500, payloadModel);
+            }
         }
     }
 }
