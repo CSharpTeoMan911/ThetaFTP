95853c62b11826ae47973ccfdc89a0559ece419b
Teodor <teodormihail07@gmail.com>
2025-04-05 23:34:55 +0100

    * UPDATED PACKAGES

--- Shared/Classes/DirectoryUploadPayloadBuilder.cs
+++ Shared/Classes/DirectoryUploadPayloadBuilder.cs
@@ -7,7 +7,7 @@ namespace ThetaFTP.Shared.Classes
 {
     public class DirectoryUploadPayloadBuilder
     {
-        public static async Task<string?> DirectoryUpload(string? directory, string? path, string? log_in_session_key) => new StringBuilder("/directories/insert?").Append(await QueryParsing.QueryParser(new DirectoryOperationMetadata()
+        public static async Task<string?> DirectoryUpload(string? directory, string? path, string? log_in_session_key) => new StringBuilder("/directories/insert?").Append(QueryParsing.QueryParser(new DirectoryOperationMetadata()
         {
             path = path,
             directory_name = FileSystemFormatter.ItemNameCharacterReplacement(directory),
